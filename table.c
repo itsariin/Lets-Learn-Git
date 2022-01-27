2c2a6e73130a5645ac4f3827e93a1f3f3fad172b
Arin Jain <arinjain349@gmail.com>
2022-01-27 19:04:19 +0530

    added some specification

--- table.c
+++ table.c
@@ -5,7 +5,7 @@ int main(){
 int num;
    printf("Enter the number for whose table you want to print  :  ");
    scanf("%d",&num); 
-    printf("The requird table is  : ");
+    printf("The requird table of  %d : ", num);
    int i;
    for(i=1; i<=10; i++){
        printf("%dx%d=%d\n",num,i,num*i);

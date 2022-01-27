335932576df3f271abd64aa5ee49070f448e15ba
Arin Jain <arinjain349@gmail.com>
2022-01-27 18:11:24 +0530

    added multiply.c

---
+++ multiply.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int num1, num2;
+    printf("Enter the numbers  :  ");
+    scanf("%d %d", &num1, &num2);
+    
+    printf("The multiplication :  %d", num1*num2);
+
+    return 0;
+}
